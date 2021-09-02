4f7045f938ddde55730cccee17c579d68935a326
rodolfopmo <rodolfo.pmo@gmail.com>
2021-09-02 20:07:31 +0000

    Exercício 3

---
+++ Exer3Semana5.java
@@ -0,0 +1,15 @@
+public class Exer3Semana5{
+    public static void main(String args []){
+        for(int i = 1; i <= 5; i ++){
+            for(int j = 1; j <= 5; j ++){
+                if(i %2 == 0){
+                System.out.print(i * j + " ");        
+                }
+
+            }
+            else if(%2 != 0){
+            System.out.print(i * j + " ");
+            }
+        }   System.out.print("\n");
+    }
+}
